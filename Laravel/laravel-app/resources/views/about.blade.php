0985adb85f59a6be742ce0534b3603811ee96122
TienTran1504 <74586033+TienTran1504@users.noreply.github.com>
2023-01-26 20:39:22 +0700

    basic syntax-command in laravel blade

--- Laravel/laravel-app/resources/views/about.blade.php
+++ Laravel/laravel-app/resources/views/about.blade.php
@@ -2,4 +2,25 @@
 
 @section('content')
 <h1> About page</h1>
+{{ $x = 20 }}
+@if ($x<2)
+    <h3> x is less than 2</h3> 
+@elseif($x <10)
+    <h3>x less than 10</h3>
+@else
+    <h3>All conditions are not match</h3>
+@endif
+{{--  --}}
+{{-- @unless(empty($name))
+    <h3> Name is not empty</h3>
+@endunless --}}
+{{-- @if (!empty($name))
+    <h3> Name is not empty</h3>
+@endif --}}f
+@empty(!$name)
+    <h3> Name is not empty</h3>
+@endempty
+@foreach ($names as $name)
+    <h3>each name: {{ $name }}</h3>
+@endforeach
 @endsection
